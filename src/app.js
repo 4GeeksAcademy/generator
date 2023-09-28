196129a7804f6640a8ed2595e3c02b385424d6ee
Arturo <141342995+amancilla88@users.noreply.github.com>
2023-09-28 00:54:31 +0000

    overwrite

--- src/app.js
+++ src/app.js
@@ -1,11 +1,39 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+window.onload = () => {
+  document.querySelector("#ursulo").addEventListener("click", () => {
+    document.querySelector("#Excuse").innerHTML = excuseGenerator();
+  });
 };
+
+function excuseGenerator() {
+  let pronoun = ["the dog", "the cat", "the frog", "the turtle", "the rabbit"];
+  let verb = ["ate", "destroyed", "played", "slept", "drank"];
+  let object = [
+    "in the bed",
+    "in the house",
+    "in the basement",
+    "in the living room",
+    "in the cage"
+  ];
+  let place = [
+    "in the park",
+    "at the school",
+    "at 4 geeks",
+    "my bedroom",
+    "in the ground"
+  ];
+  let time = [
+    "yesterday",
+    "in the morning",
+    "two hours ago",
+    "coming to class",
+    "on monday"
+  ];
+
+  const pronounIndex = Math.floor(Math.random() * pronoun.length);
+  const verbIndex = Math.floor(Math.random() * verb.length);
+  const objectIndex = Math.floor(Math.random() * object.length);
+  const placeIndex = Math.floor(Math.random() * place.length);
+  const timeIndex = Math.floor(Math.random() * time.length);
+
+  return `${pronoun[pronounIndex]} ${verb[verbIndex]} ${object[objectIndex]} ${place[placeIndex]} ${time[timeIndex]} .`;
+}
